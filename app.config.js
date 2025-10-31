a10d83ffc7563b864005d183dd5444056acc3886
caiosoares1 <batistacaio2244@gmail.com>
2025-10-31 15:05:23 -0300

    chore: adicionar build automatico e projectId do EAS

--- app.config.js
+++ app.config.js
@@ -8,10 +8,18 @@ export default {
     "scheme": "gymapp",
     "userInterfaceStyle": "automatic",
     "newArchEnabled": true,
+    "runtimeVersion": {
+      "policy": "appVersion"
+    },
+    "updates": {
+      "url": "https://u.expo.dev/39f0221e-8724-41b3-ad27-7979b9525298"
+    },
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "bundleIdentifier": "com.caiosoares1.gymapp"
     },
     "android": {
+      "package": "com.caiosoares1.gymapp",
       "adaptiveIcon": {
         "backgroundColor": "#E6F4FE",
         "foregroundImage": "./assets/images/android-icon-foreground.png",
@@ -46,7 +54,10 @@ export default {
     },
     "extra": {
       "supabaseUrl": process.env.SUPABASE_URL,
-      "supabaseAnonKey": process.env.SUPABASE_ANON_KEY
+      "supabaseAnonKey": process.env.SUPABASE_ANON_KEY,
+      "eas": {
+        "projectId": "39f0221e-8724-41b3-ad27-7979b9525298"
+      }
     }
   }
 }
